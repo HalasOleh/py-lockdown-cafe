ae48f5443f07b1f3a29b09aedfe373242c48bf97
Oleh Halas <oleggalas8@gmail.com>
2026-03-02 16:09:25 +0200

    Implement Class Cafe. Add custom errros. Implement logic try exept

---
+++ app/cafe.py
@@ -0,0 +1,35 @@
+import datetime
+from .errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError,
+)
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        vaccine = visitor.get("vaccine")
+
+        if not vaccine:
+            raise NotVaccinatedError(
+                f"{visitor.get('name', 'Visitor')} must be vaccinated"
+            )
+
+        vaccine_date = vaccine.get("expiration_date")
+
+        if vaccine_date < datetime.date.today():
+            raise OutdatedVaccineError(
+                f"{visitor.get('name', 'Visitor')}'s vaccine is outdated"
+            )
+
+        wearing_a_mask = visitor.get("wearing_a_mask")
+
+        if not wearing_a_mask:
+            raise NotWearingMaskError(
+                f"{visitor.get('name', 'Visitor')} must wear the mask"
+            )
+
+        return f"Welcome to {self.name}"
---
+++ app/errors.py
@@ -0,0 +1,15 @@
+class VaccineError(Exception):
+    """Base class for vaccine-related errors"""
+    pass
+
+
+class NotVaccinatedError(VaccineError):
+    pass
+
+
+class OutdatedVaccineError(VaccineError):
+    pass
+
+
+class NotWearingMaskError(Exception):
+    pass
